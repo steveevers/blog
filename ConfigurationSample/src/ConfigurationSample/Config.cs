3d497634bb65ec7d95c7b3b3678eae956e9070af
americas\St0y1j8 <steven_evers@gap.com>
2017-01-10 14:35:19 -0800

    Importing ConfigurationSample

---
+++ ConfigurationSample/src/ConfigurationSample/Config.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConfigurationSample
+{
+    public class Config {
+        public int MyNumberSetting
+        {
+            get { return MyNumber; }
+            set { MyNumber = value; }
+        }
+
+        public string MyTextSetting
+        {
+            get { return MyText; }
+            set { MyText = value; }
+        }
+
+        public static int MyNumber { get; private set; }
+        public static string MyText { get; private set; }
+    }
+}
---
+++ ConfigurationSample/src/ConfigurationSample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace ConfigurationSample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("Config.json")
+                .AddJsonFile("Config.Debug.json", optional: true)
+                //  .AddJsonFile("Config.[anything].json", optional: true)
+                .Build()
+                .GetSection("Config")
+                .Bind(new Config());
+
+            Console.WriteLine(Config.MyNumber);
+            Console.ReadKey();
+        }
+    }
+}
